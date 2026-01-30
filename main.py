716cd3cf1a84455bc2e69c2d409a5d0b2054870a
WhileTrueDoNothing <onesandzeroes@nym.hush.com>
2026-01-30 22:09:20 +0000

    Added recursion until first numbers are reached

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def first_fib(num1, num2):
     if num2-num1 > num1:
-        return(num1, num2)
+        return (num1, num2)
     else:
-        return((num2-num1), num1)
+        return first_fib((num2-num1), num1)
